410a11804807508c172fbd2403022822b2a09108
Thomas Jackson <tom.jackson314@gmail.com>
2020-09-04 17:33:35 -0400

    Initial commit of XML->JSON parser script

---
+++ py_cf_metadata/classdefs.py
@@ -0,0 +1,282 @@
+import bisect
+import collections
+import collections.abc
+import copy
+import dataclasses as dc
+import datetime
+import html
+from typing import List
+import warnings
+
+class CFBase(object):
+    # Tag type in the CF convention tables to use to initialize object instances
+    # in the vaf_from_xml() method (defined in child classes.)
+    xml_tag = "entry"
+
+    def __post_init__(self):
+        """Post-processing of field values.
+        """
+        for fld in dc.fields(self):
+            if fld.type == str:
+                # cleanup for all string-valued fields
+                key = fld.name
+                val = self.__getattribute__(key)
+                val = ("" if val is None else val)
+                # Convert html-escaped characters (eg. '&quot;') to unicode
+                # and strip leading and trailing whitespace
+                val = html.unescape(val).strip()
+                # replace multiple spaces with single space
+                val = ' '.join(val.split())
+                self.__setattr__(key, val)
+
+    @classmethod
+    def key_from_xml(cls, xml_entry):
+        return xml_entry.get('id')
+
+    @classmethod
+    def val_from_xml(cls, xml_entry, **kwargs):
+        """Initialize dataclass instance from an XML entry (from the CF 
+        convention tables.) Field entries not present in the XML can be passed
+        as arbitrary kwargs.
+        """
+        for fld in dc.fields(cls):
+            key = fld.name
+            if fld.metadata.get('xml_key', ''):
+                xml_key = fld.metadata['xml_key']
+            else:
+                xml_key = key
+            # Try to set field's value from an xml tag under this one
+            xml_val = xml_entry.find(xml_key)
+            if xml_val is not None:
+                kwargs[key] = xml_val.text
+            else:
+                # try to set field's value from this tag's attributes
+                xml_val = xml_entry.get(xml_key)
+                if xml_val is not None:
+                    kwargs[key] = xml_val
+        return cls.from_struct(kwargs)
+
+    @classmethod
+    def from_struct(cls, d):
+        return cls(**d)
+
+    def to_struct(self):
+        """Return representation as a dict, eliminating blank entries.
+        """
+        d = dc.asdict(self)
+        return {k:v for k,v in d.items() if v}
+
+    def update(self, other):
+        """Update the values of all non-versioned fields (those with compare=
+        False) from the values in other.
+        """
+        assert self.__class__ == other.__class__
+        for fld in dc.fields(self):
+            key = fld.name
+            if fld.compare:
+                # raise error when we try to change the value of a versioned
+                # field.
+                if not self.__getattribute__(key) == other.__getattribute__(key):
+                    raise ValueError(
+                        "{}: Incompatible update of {}.\nCurrent: {}\nNew: {}".format(
+                            self.__class__.__name__, key,
+                            dc.asdict(self), dc.asdict(other)
+                    ))
+            else:
+                # just update the field value.
+                self.__setattr__(key, other.__getattribute__(key))
+
+    def is_empty(self):
+        for fld in dc.fields(self):
+            if self.__getattribute__(fld.name):
+                return False
+        return True
+
+
+@dc.dataclass
+class RevisionHistoryWrapper():
+    revisions: list
+    revision_start: list
+    revision_end: list
+    WrappedDataclass: dc.InitVar = None
+
+    uninit_v = -1 # sentinel value for uninitialized/unset version number
+
+    @classmethod
+    def from_struct(cls, struct):
+        obj = cls(revisions=[], revision_start=[], revision_end=[])
+        for d in struct:
+            start_v = d.pop('revision_start')
+            end_v = d.pop('revision_end', cls.uninit_v)
+            wrapped_obj = cls.WrappedDataclass.from_struct(d)
+            obj.new_revision(wrapped_obj, start_v, end_v)
+        return obj
+
+    def to_struct(self):
+        def _rev_to_dict(wrapped_obj, start, end):
+            d = wrapped_obj.to_struct()
+            d['revision_start'] = start
+            if end != self.uninit_v:
+                d['revision_end'] = end
+            return d
+
+        return [_rev_to_dict(*tup) for tup \
+            in zip(self.revisions, self.revision_start, self.revision_end)]
+
+    @classmethod
+    def from_obj(cls, wrapped_obj, start_v):
+        return cls(
+            revisions=[wrapped_obj], 
+            revision_start=[start_v], revision_end=[cls.uninit_v],
+            WrappedDataclass=wrapped_obj.__class__
+        )
+
+    def new_revision(self, new_rev, current_v, end_v=None):
+        if len(self.revision_end) > 0 and end_v is None:
+            self.end_revision(current_v)
+        self.revisions.append(new_rev)
+        self.revision_start.append(current_v)
+        if end_v is None:
+            self.revision_end.append(self.uninit_v)
+        else:
+            self.revision_end.append(end_v)
+
+    def end_revision(self, current_v):
+        if self.revision_end[-1] == self.uninit_v:
+            self.revision_end[-1] = current_v - 1
+
+    def update(self, x, current_v):
+        current_rev = self.revisions[-1]
+        current_rev_backup = copy.copy(self.revisions[-1])
+        try:
+            # try to update our most recent revision to x.
+            current_rev.update(x)
+        except ValueError:
+            # print("update.\nCurrent: {}\nNew: {}".format(
+            #     dc.asdict(current_rev_backup), dc.asdict(x)))
+            # if we're here, update() threw an exception because x differs 
+            # from our current revision in relevant ways (compare==True), 
+            # so it should be treated as a new revision.
+            self.revisions[-1] = current_rev_backup
+            self.new_revision(x, current_v)
+
+    def remove_empty_revisions(self):
+        is_empty = [rev.is_empty() for rev in self.revisions]
+        self.revisions = [rev for rev, empty \
+            in zip(self.revisions, is_empty) if not empty]
+        self.revision_start = [v for v, empty \
+            in zip(self.revision_start, is_empty) if not empty]
+        self.revision_end = [v for v, empty \
+            in zip(self.revision_end, is_empty) if not empty]
+
+    def is_empty(self):
+        return (len(self.revisions) == 0)
+
+    def filter_by_version(self, version):
+        not_current = (self.revision_end[-1] != self.uninit_v)
+        idx = bisect.bisect_right(self.revision_start, version)
+        if idx == 0:
+            # before earliest revision containing this entry
+            return None
+        elif idx == len(self.revision_start) \
+            and not_current and version > self.revision_end[-1]:
+            # after last revision to contain this entry
+            return None
+        else:
+            return self.revisions[idx-1]
+
+    def filter_by_recent(self):
+        return self.revisions[-1]
+
+# --------------------------------------------------------------
+
+def warn_on_defaults_factory(field_info, default_value):
+    """Returns a function that can be used as a defaults_factory for dataclasses 
+    that raises a warning before setting a field to a default value.
+    """
+    def _dummy_func():
+        warnings.warn("No {} recieved, using default {}".format(
+            field_info, default_value
+        ))
+        return default_value
+    return _dummy_func
+
+canonical_units_default = warn_on_defaults_factory("StandardName.canonical_units", "1")
+@dc.dataclass
+class StandardName(CFBase):
+    canonical_units: str = dc.field(default_factory=canonical_units_default)
+    description: str = dc.field(default="", compare=False)
+
+@dc.dataclass
+class AreaType(CFBase):
+    description: str = dc.field(default="", compare=False)
+
+@dc.dataclass
+class StandardizedRegion(CFBase):
+    description: str = dc.field(default="", compare=False)
+
+@dc.dataclass
+class StandardNameAlias(CFBase):
+    xml_tag = "alias"
+    alias: str = dc.field(default="", metadata={'xml_key':'entry_id'})
+
+@dc.dataclass
+class StandardNameAliasAMIP(CFBase):
+    alias: str = dc.field(default="", metadata={'xml_key':'amip'})
+
+@dc.dataclass
+class StandardNameAliasGRIB(CFBase):
+    alias: str = dc.field(default="", metadata={'xml_key':'grib'})
+
+# --------------------------------------------------------------
+
+class RevisionDateList(object):
+    def __init__(self, d=None):
+        if d is None:
+            self._d = dict()
+        else:
+            self._d = d
+
+    @classmethod
+    def from_struct(cls, d):
+        return cls({
+            k: datetime.date.fromisoformat(v) for k,v in d.items()
+        })
+
+    def to_struct(self):
+        return {k: v.isoformat() for k, v in self._d.items()}
+
+    def add_modified_date_from_xml(self, xml_root, current_version):
+        val = None
+        fld = xml_root.find('last_modified')
+        if fld is not None and val is None:
+            # current date tag format used by CF conventions
+            try:
+                dt = datetime.datetime.strptime(fld.text, '%Y-%m-%dT%H:%M:%SZ')
+            except ValueError:
+                dt = datetime.datetime.strptime(fld.text, '%Y-%m-%dT%H:%MZ')
+            val = dt.date()
+        fld = xml_root.find('date')
+        if fld is not None and val is None:
+            # older date tag format used by CF conventions
+            dt = datetime.datetime.strptime(fld.text, '%d %B %Y')
+            val = dt.date()
+        if val is None:
+            warnings.warn("Couldn't find modification date in file {}.".format(
+                current_version))
+            val = datetime.date(1900,1,1) # dummy value so that lookup will work
+        self._d[str(current_version)] = val
+
+    def date_from_version(self, version):
+        return self._d.get(str(version), None)
+
+    def version_from_date(self, dt):
+        keys = list(self._d.keys())
+        dts = list(self._d.values())
+        if dts != sorted(dts):
+            raise ValueError("Revision dates not in sorted order; lookup will fail")
+        idx = bisect.bisect_right(dts, dt)
+        if idx == 0:
+            return keys[0]
+        else:
+            return keys[idx - 1]
---
+++ update.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+
+import sys
+# do version check before importing other stuff
+if sys.version_info[0] != 3 or sys.version_info[1] < 7:
+    print(("ERROR: Update script only supports python >= 3.7. Please check "
+    "which version is on your $PATH (e.g. with `which python`.)"))
+    print("Attempted to run with following python version:\n{}".format(sys.version))
+    exit()
+# passed; continue with imports
+import os
+import argparse
+import abc
+import datetime
+import glob
+import json
+import subprocess
+import warnings
+import xml.etree.ElementTree as ET
+
+from py_cf_metadata.classdefs import (
+    StandardName, AreaType, StandardizedRegion,
+    StandardNameAlias, StandardNameAliasAMIP, StandardNameAliasGRIB,
+    RevisionHistoryWrapper,
+    RevisionDateList
+)
+
+class XMLProcessor(abc.ABC):
+    dict_factory = dict
+
+    def __init__(self, xml_dir, DataClass):
+        self.xml_dir = xml_dir
+        self.DataClass = DataClass
+        self.rev_history_d = self.dict_factory()
+        self.rev_dates = RevisionDateList()
+        self.max_version = -1
+
+    @abc.abstractmethod
+    def xml_path(self, version_str):
+        pass
+
+    def get_versions(self, path):
+        with os.scandir(path) as iter_:
+            version_dirs = [d.name for d in iter_ if \
+                d.is_dir() and not (d.name.startswith('.') or d.name == 'docs')]
+        return self.v_cleanup(version_dirs)
+
+    @staticmethod
+    def v_cleanup(version_strs):
+        assert 'current' in version_strs
+        version_strs.remove('current')
+        version_ints = sorted([int(d) for d in version_strs])
+        max_version = max(version_ints)
+        version_strs = [str(d) for d in version_ints] + ['current']
+        return (version_strs, max_version)
+
+    @staticmethod
+    def strip_namespaces(file_):
+        """Load an XML file and strip namespace information (to simplify parsing.)
+        Taken from `https://stackoverflow.com/a/25920989`__.
+        """
+        it = ET.iterparse(file_)
+        for _, el in it:
+            for _, el in it:
+                _, _, el.tag = el.tag.rpartition('}') # strip namespaces
+            for at in el.attrib: # strip namespaces of attributes too
+                if '}' in at:
+                    newat = at.split('}', 1)[1]
+                    el.attrib[newat] = el.attrib[at]
+                    del el.attrib[at]
+        return it.root
+
+    def dict_update(self, update_d, update_version):
+        d_keys = set(self.rev_history_d.keys())
+        update_keys = set(update_d.keys())
+        for k in d_keys.intersection(update_keys):
+            # update entries in both ref_d and update_d: update only the fields
+            # that we've decided to update.
+            try:
+                self.rev_history_d[k].update(update_d[k], update_version)
+            except ValueError as exc:
+                print(exc)
+                continue
+            #print("\tUpdate: ",k, update_d[k].version_added, ref_d[k].version_added)
+        for k in d_keys.difference(update_keys):
+            # entries in ref_d that aren't in update_d: 
+            self.rev_history_d[k].end_revision(update_version)
+        for k in update_keys.difference(d_keys):
+            # entries in update_d not in ref_d : add them.
+            if update_version == 0:
+                # data in "current" should be a duplicate of most current 
+                # numbered version, so shouldn't be adding anything then
+                warnings.warn("Modifications made to {} in 'current'".format(k))
+            if k in self.rev_history_d and self.rev_history_d[k] != update_d[k]:
+                # if we already added this entry, that's a problem
+                #raise KeyError(
+                warnings.warn(
+                    'Multiple assignments to {} in version {}\nCurrent: {}\nNew: {}'.format(
+                        k, update_version, 
+                        self.rev_history_d[k].to_struct(), update_d[k].to_struct()))
+            self.rev_history_d[k] = \
+                RevisionHistoryWrapper.from_obj(update_d[k], update_version)
+            # print("\tAdd: {} ({})\nCurrent: {}\nNew: {}".format(
+            #     k, update_version, self.rev_history_d[k], update_d[k]))
+
+    def process(self):
+        v_strs, self.max_version = self.get_versions(self.xml_dir)
+        for v_str in v_strs:
+            v_int = (0 if v_str == 'current' else int(v_str))
+            print(v_str, end=" ")
+            xml_path = self.xml_path(v_str)
+            assert os.path.exists(xml_path)
+            root = self.strip_namespaces(xml_path)
+            self.rev_dates.add_modified_date_from_xml(root, v_str)
+            
+            update_d = dict()
+            for x in root.iterfind(self.DataClass.xml_tag):
+                k = self.DataClass.key_from_xml(x)
+                val = self.DataClass.val_from_xml(x)
+                if k in update_d and update_d[k] != val:
+                    #raise KeyError(
+                    warnings.warn(
+                        'Multiple assignments to {} in file {}\nCurrent: {}\nNew: {}'.format(
+                            k, v_str, update_d[k].to_struct(), val.to_struct()))
+                update_d[k] = val
+            self.dict_update(update_d, v_int)
+        print()
+
+        # remove completely empty entries
+        for val in self.rev_history_d.values():
+            val.remove_empty_revisions()
+        self.rev_history_d = {k:v for k,v in self.rev_history_d.items() \
+            if not v.is_empty()}
+
+        # sort by keys; all dicts in py3.7+ are OrderedDicts
+        self.rev_history_d = dict(sorted(self.rev_history_d.items()))
+
+class StdNameXMLProcessor(XMLProcessor):
+    def xml_path(self, v_str):
+        return os.path.join(
+            self.xml_dir, v_str, 'src', 'cf-standard-name-table.xml'
+        )
+
+class AreaTypeXMLProcessor(XMLProcessor):
+    def xml_path(self, v_str):
+        return os.path.join(
+            self.xml_dir, v_str, 'src', 'area-type-table.xml'
+        )
+
+class StdRegionXMLProcessor(XMLProcessor):
+    def get_versions(self, path):
+        glob_ = 'standardized-region-list.*.xml'
+        files = glob.glob(os.path.join(path, glob_))
+        v_strs = [os.path.basename(f).split('.')[1] for f in files]
+        return self.v_cleanup(v_strs)
+
+    def xml_path(self, v_str):
+        return os.path.join(
+            self.xml_dir, 'standardized-region-list.'+v_str+'.xml'
+        )
+
+# --------------------------------------------------------------
+
+def git_info(repo_dir=None):
+    """Get the current git branch, hash, and list of uncommitted files, if 
+    available. Based on NumPy's implementation: 
+    `https://stackoverflow.com/a/40170206`__.
+    """
+    def _minimal_cmd(cmd):
+        # construct minimal environment
+        env = {'LANGUAGE':'C', 'LANG':'C', 'LC_ALL':'C'}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        try:
+            out = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, 
+                env=env, cwd=repo_dir
+            ).communicate()[0]
+        except subprocess.CalledProcessError:
+            out = ''
+        return out.strip().decode('utf-8')
+
+    git_branch = ""
+    git_hash = ""
+    git_dirty = ""
+    try:
+        git_branch = _minimal_cmd(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+        git_hash = _minimal_cmd(['git', 'rev-parse', 'HEAD'])
+        git_dirty = _minimal_cmd(['git', 'diff', '--no-ext-diff', '--name-only'])
+    except OSError:
+        pass
+        
+    if not git_hash:
+        git_hash = "<couldn't get git hash>"
+    if not git_branch:
+        git_branch = "<couldn't get git branch>"
+    if git_dirty:
+        git_branch = git_branch + " (with uncommitted changes)"
+    return (git_hash, git_branch)
+
+def provenance_dict(repo_dir, upstream_repo_dir):
+    now_ = datetime.datetime.now(tz=datetime.timezone.utc)
+    my_hash, my_branch = git_info(repo_dir)
+    up_hash, up_branch = git_info(upstream_repo_dir)
+    return {
+        'last_modified': now_.isoformat(),
+        'tsjackson-noaa/cf-convention-data': {
+            'hash': my_hash, 'branch': my_branch
+        },
+        'cf-convention/cf-convention.github.io': {
+            'hash': up_hash, 'branch': up_branch
+        }
+    }
+
+def write_json(dir_, category_name, xml_proc, prov_d):
+    path = os.path.join(dir_, category_name+'.json')
+    if os.path.exists(path):
+        print('Overwriting {}'.format(path))
+    prov_copy = prov_d.copy()
+    prov_copy['current_cf_revision'] = str(xml_proc.max_version)
+    prov_copy['dataclass'] = xml_proc.DataClass.__name__
+    out_d = {
+        'provenance': prov_copy,
+        'revision_dates': xml_proc.rev_dates.to_struct(),
+        category_name: {k: v.to_struct() \
+            for k, v in xml_proc.rev_history_d.items() if v is not None}
+    }
+    with open(path, 'w') as f:
+        json.dump(out_d, f, sort_keys=False, indent=2, ensure_ascii=True)
+
+# --------------------------------------------------------------
+
+def main(my_repo_dir, cf_repo_dir):
+    out_dir = os.path.join(my_repo_dir, 'tables')
+    os.makedirs(out_dir, exist_ok=True)
+    prov_d = provenance_dict(my_repo_dir, cf_repo_dir)
+
+    std_name_dir = os.path.join(cf_repo_dir, 'Data', 'cf-standard-names')
+    std_name_proc = StdNameXMLProcessor(std_name_dir, StandardName)
+    std_name_proc.process()
+    write_json(out_dir, 'cf-standard-names', std_name_proc, prov_d)
+
+    std_name_proc = StdNameXMLProcessor(std_name_dir, StandardNameAlias)
+    std_name_proc.process()
+    write_json(out_dir, 'cf-standard-name-aliases', std_name_proc, prov_d)
+
+    std_name_proc = StdNameXMLProcessor(std_name_dir, StandardNameAliasAMIP)
+    std_name_proc.process()
+    write_json(out_dir, 'cf-standard-name-aliases-amip', std_name_proc, prov_d)
+
+    std_name_proc = StdNameXMLProcessor(std_name_dir, StandardNameAliasGRIB)
+    std_name_proc.process()
+    write_json(out_dir, 'cf-standard-name-aliases-grib', std_name_proc, prov_d)
+
+    area_type_dir = os.path.join(cf_repo_dir, 'Data', 'area-type-table')
+    area_type_proc = AreaTypeXMLProcessor(area_type_dir, AreaType)
+    area_type_proc.process()
+    write_json(out_dir, 'area-type-table', area_type_proc, prov_d)
+
+    std_region_dir = os.path.join(cf_repo_dir, 'Data', 'standardized-region-list')
+    std_region_proc = StdRegionXMLProcessor(std_region_dir, StandardizedRegion)
+    std_region_proc.process()
+    write_json(out_dir, 'standardized-region-list', std_region_proc, prov_d)
+
+# --------------------------------------------------------------
+
+if __name__ == '__main__':
+    # Wrap input/output if we're called as a standalone script
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true",
+        help="increase output verbosity")
+    parser.add_argument("cf_repo_dir", 
+        help="Path to the local checkout of cf-convention/cf-convention.github.io.")
+    args = parser.parse_args()
+
+    my_repo_dir = os.path.dirname(os.path.realpath(__file__))
+    cf_repo_dir = args.cf_repo_dir
+    assert os.path.exists(cf_repo_dir)
+
+    main(my_repo_dir, cf_repo_dir)
